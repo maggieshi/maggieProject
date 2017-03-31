0685cc8e5f4e0fb4a0d58bb1e10b856f540f583b
Maggie Shi <maggieshi@fb.com>
2017-03-31 15:39:38 +1100

    no message

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'abc123') {
   echo $challenge;
 }
-error_log(print_r('webhook called');
+//error_log(print_r('webhook called');
 //error_log(var_dump( file_get_contents('php://input')));
 
 $input = json_decode(file_get_contents('php:://input'), true);
